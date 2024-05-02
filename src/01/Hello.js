647a2515dbe47811f9cdd06f5689da4185049f11
pnumin <pnumin@pusan.ac.kr>
2024-05-02 17:57:39 +0900

    리액트 개요

---
+++ src/01/Hello.js
@@ -0,0 +1,14 @@
+function Hello() {
+  return (
+    <>
+      <p>
+        K-digital 7기 김경민
+      </p>
+      <ul>
+        <li>리액트 컴포넌트</li>
+      </ul>
+    </>
+  );
+}
+
+export default Hello;
