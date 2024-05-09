9df76ce457338e372c788058bf44d9b799d04062
pnumin <pnumin@pusan.ac.kr>
2024-05-09 11:00:51 +0900

    박스오피스 테이블완성

--- src/06/BoxOffice.js
+++ src/06/BoxOffice.js
@@ -1,47 +1,17 @@
 import box from "./BoxOffice.json";
+import BoxOfficeThead from "./BoxOfficeThead";
+import BoxOfficeTbody from "./BoxOfficeTbody";
+
 export default function BoxOffice() {
   const dailyList = box.boxOfficeResult.dailyBoxOfficeList;
-  const tags = dailyList.map(item =>
-    <li key={item.movieCd}>
-      [ {item.rank} ] {item.movieNm}
-    </li>);
+  
   return (
     <div className="w-full h-full">
       <div className="w-full flex flex-col justify-start items-center mt-10">
         <table
           className="w-11/12 text-left text-sm font-light text-surface">
-          <thead
-            className="border-b border-neutral-200 font-medium">
-            <tr>
-              <th scope="col" className="px-6 py-4">#</th>
-              <th scope="col" className="px-6 py-4">First</th>
-              <th scope="col" className="px-6 py-4">Last</th>
-              <th scope="col" className="px-6 py-4">Handle</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              className="border-b border-neutral-200 hover:bg-neutral-100 ">
-              <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
-              <td className="whitespace-nowrap px-6 py-4">Mark</td>
-              <td className="whitespace-nowrap px-6 py-4">Otto</td>
-              <td className="whitespace-nowrap px-6 py-4">@mdo</td>
-            </tr>
-            <tr
-              className="border-b border-neutral-200  hover:bg-neutral-100">
-              <td className="whitespace-nowrap px-6 py-4 font-medium">2</td>
-              <td className="whitespace-nowrap px-6 py-4">Jacob</td>
-              <td className="whitespace-nowrap px-6 py-4">Thornton</td>
-              <td className="whitespace-nowrap px-6 py-4">@fat</td>
-            </tr>
-            <tr
-              className="border-b border-neutral-200  hover:bg-neutral-100">
-              <td className="whitespace-nowrap px-6 py-4 font-medium">3</td>
-              <td className="whitespace-nowrap px-6 py-4">Larry</td>
-              <td className="whitespace-nowrap px-6 py-4">Wild</td>
-              <td className="whitespace-nowrap px-6 py-4">@twitter</td>
-            </tr>
-          </tbody>
+          <BoxOfficeThead />
+          <BoxOfficeTbody dailyList = {dailyList} />
         </table>
       </div>
     </div>
---
+++ src/06/BoxOfficeTbody.js
@@ -0,0 +1,29 @@
+import { AiFillCaretUp } from "react-icons/ai";
+import { AiFillCaretDown } from "react-icons/ai";
+export default function BoxOfficeTbody({ dailyList }) {
+  console.log(dailyList)
+  const tags = dailyList.map(item =>
+    <tr
+      className="border-b border-neutral-200 hover:bg-neutral-100 ">
+      <td className="whitespace-nowrap px-6 py-2 font-medium">{item.rank}</td>
+      <td className="whitespace-nowrap px-6 py-2">{item.movieNm}</td>
+      <td className="whitespace-nowrap px-6 py-2 text-right">{parseInt(item.salesAcc).toLocaleString()}</td>
+      <td className="whitespace-nowrap px-6 py-2 text-right">{parseInt(item.audiAcc).toLocaleString()}</td>
+      <td className="px-2 py-2 flex justify-center items-center font-bold">
+        <span>{item.rankInten > 0 ? <AiFillCaretUp className="text-red-600"/> : 
+             item.rankInten < 0 ? <AiFillCaretDown className="text-blue-600"/> : '-' }
+        </span>
+        <span>
+        {parseInt(item.rankInten) !== 0 && Math.abs(item.rankInten)}
+        </span>
+      </td>
+    </tr>
+
+  );
+
+  return (
+    <tbody>
+      {tags}
+    </tbody>
+  )
+}
